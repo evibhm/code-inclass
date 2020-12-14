a23c022489c32bf279bb9b2768227eae0ec9dac4
evibhm <evibhm@protonmail.com>
2020-12-14 15:12:48 +0800

    add stl_vector

--- exercise4-function_and_class/2.cpp
+++ exercise4-function_and_class/2.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 
 bool isPrime(int num)
 {
@@ -10,13 +11,13 @@ bool isPrime(int num)
 }
 
 int main(){
-    int arr[800] = {5, 7, 11};
-    int palindrome, ans = 2;
+    std::vector<int> arr = {5, 7, 11};
+    int palindrome;
     for (int d1 = 1; d1 < 10; d1+=2) {
         for (int d2 = 0; d2 < 10; ++d2) {
             palindrome = 100*d1 + 10*d2 + d1;
             if (isPrime(palindrome)) {
-                arr[++ans] = palindrome;
+                arr.push_back(palindrome);
             }
         }
     }
@@ -25,7 +26,7 @@ int main(){
             for (int d3 = 0; d3 < 10; ++d3) {
                 palindrome = 10000*d1 + 1000*d2 +100*d3 + 10*d2 + d1;
                 if (isPrime(palindrome)) {
-                    arr[++ans] = palindrome;
+                    arr.push_back(palindrome);
                 }
             }
         }
@@ -36,7 +37,7 @@ int main(){
                 for (int d4 = 0; d4 < 10; ++d4){
                     palindrome = 1000000*d1 + 100000*d2 + 10000*d3 + 1000*d4 +100*d3 + 10*d2 + d1;
                     if (isPrime(palindrome)) {
-                        arr[++ans] = palindrome;
+                        arr.push_back(palindrome);
                     }
                 }
             }
