10ced6f02baae50697584b9c39f5e9c4b323178a
evibhm <evibhm@protonmail.com>
2021-04-09 20:22:42 +0800

    完善逻辑

--- py-exercise1/2.py
+++ py-exercise1/2.py
@@ -2,10 +2,15 @@ denominations = (50, 20, 10, 5, 1)
 
 print("Please enter the amount payable and the actual amount paid:")
 amount_payable, amount_actual = [eval(i) for i in input().split()]
-amount_change = amount_actual - amount_payable
-print("Should get change: {} yuan".format(amount_change))
-ac = amount_change
-for den in denominations:
-    if ac >= den:
-        print("{} yuan banknotes * {}".format(den, ac // den))
-        ac %= den
+if amount_actual < amount_payable:
+    print("It is not enough, Please give me more money.")
+elif amount_actual == amount_payable:
+    print("It is suitable, Welcome next time.")
+else:
+    amount_change = amount_actual - amount_payable
+    print("Should get change: {} yuan".format(amount_change))
+    ac = amount_change
+    for den in denominations:
+        if ac >= den:
+            print("{} yuan banknotes * {}".format(den, ac // den))
+            ac %= den
--- py-exercise1/4.py
+++ py-exercise1/4.py
@@ -5,15 +5,17 @@ while True:
         if len(side) != 3:
             print("There are no three input data")
             continue
-        if side[0] + side[1] > side[2] and side[1] + side[2] > side[0]\
-           and side[2] + side[0] > side[1]:
+        if side[0] > 0 and side[1] > 0 and side [2] > 0 \
+        and side[0] + side[1] > side[2] and side[1] + side[2] > side[0]\
+        and side[2] + side[0] > side[1]:
+            p = sum(side) / 2
+            area = p
+            for i in side:
+                area *= p - i
+            area = area ** 0.5
+            print("Area: {:.2f}".format(area))
             break
         print("ERROR: The input side length cannot form a triangle.")
     except(NameError, SyntaxError):
         print("ERROR: The input data is not a real number.")
-p = sum(side) / 2
-area = p
-for i in side:
-    area *= p - i
-area = area ** 0.5
-print("Area: {:.2f}".format(area))
+
